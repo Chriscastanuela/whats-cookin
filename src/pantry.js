0ea48d223c4a4520e0b8542fb8a1a22af4398265
Chriscastanuela <chriscastanuela@ymail.com>
2020-09-05 15:05:50 -0600

    Add first pantry test pass

--- src/pantry.js
+++ src/pantry.js
@@ -1,7 +1,28 @@
+import recipeData from "./data/recipes";
+
 class Pantry {
   constructor(userIngredients) {
     this.contents = userIngredients;
   }
+  doIHaveEnoughForThis(nameOrId) {
+    let toggle = "Yes";
+    let thisRecipe = recipeData.find(index => {
+      return index.id || index.name == nameOrId;
+    }).ingredients;
+    console.log("Ingredients", thisRecipe);
+    thisRecipe.forEach(index => {
+      if (!this.contents[index]) {
+        toggle = "No"
+      } else {
+        this.contents.forEach(i => {
+          if (i.amount >= index.quantity.amount) {
+            toggle == "Yes";
+          }
+        })
+      }
+    });
+    return toggle;
+  };
 }
 
 export default Pantry;
