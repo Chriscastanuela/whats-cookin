d3e84be80d5789fb66de50fb5f8a7fd4b0821d6e
Bret Merritt <merrittbret9@gmail.com>
2020-09-11 19:18:41 -0600

    Reformat html rendering methods for readablity

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -17,25 +17,26 @@ class DomUpdates {
       cardArea.innerHTML = '';
       user.favoriteRecipes.forEach(recipe => {
         console.log(user.favoriteRecipes);
-        cardArea.insertAdjacentHTML('afterbegin', `<article id='${recipe.id}-card'
-        class='card'>
-        <header id='${recipe.id}-header' class='card-header'>
-        <label for='add-button' class='hidden'>Click to add recipe</label>
-        <button id='${recipe.id}-add' aria-label='add-button' class='add-button card-button'>
-        <label for='favorite-button' class='hidden'>Click to favorite recipe
-        </label>
-        <button id='${recipe.id}-favorite-button' aria-label='favorite-button' class='favorite favorite-active card-button'>
-        </button></header>
-        <article id='${recipe.id}-recipie-name' class='recipe-name'>${recipe.name}</span>
-        <img id='${recipe.id}-picture' tabindex='0' class='card-picture'
-        src='${recipe.image}' alt='Food from recipe'>
-        </article>`)
-      })
+        cardArea.insertAdjacentHTML('afterbegin', 
+          `<article id='${recipe.id}-card' class='card'>
+            <header id='${recipe.id}-header' class='card-header'>
+              <label for='add-button' class='hidden'>Click to add recipe</label>
+              <button id='${recipe.id}-add' aria-label='add-button' class='add-button card-button'>
+              <label for='favorite-button' class='hidden'>Click to favorite recipe
+              </label>
+              <button id='${recipe.id}-favorite-button' aria-label='favorite-button' class='favorite favorite-active card-button'>
+              </button>
+            </header>
+            <article id='${recipe.id}-recipie-name' class='recipe-name'>${recipe.name}</article>
+            <img id='${recipe.id}-picture' tabindex='0' class='card-picture' src='${recipe.image}' alt='Food from recipe'>
+          </article>`)
+      });
     }
   }
 
   greetUser() {
     const userName = document.querySelector('.user-name');
+    //should pull this querySelector out of function into global
     userName.innerHTML =
     user.name.split(' ')[0] + ' ' + user.name.split(' ')[1][0];
   }
@@ -45,7 +46,6 @@ class DomUpdates {
     if (event.target.id.includes('-')) {
       targetedID = event.target.id.slice(0, event.target.id.indexOf('-'))
           // i think we can delete this if conditional and just have the above line
-  
     } else {
       targetedID = event.target.id
     }
@@ -57,6 +57,7 @@ class DomUpdates {
       }
     })
     console.log(specificRecipe);
+    // remove this console log
     if (!event.target.classList.contains('favorite-active')) {
       event.target.classList.add('favorite-active');
       favButton.innerHTML = 'View Favorites';
@@ -95,14 +96,20 @@ class DomUpdates {
     let cost = recipeObject.calculateCost()
     let costInDollars = (cost / 100).toFixed(2)
     cardArea.classList.add('all');
-    cardArea.innerHTML = `<h3>${recipeObject.name}</h3>
-    <p class='all-recipe-info'>
-    <strong>It will cost: </strong><span class='cost recipe-info'>
-    $${costInDollars}</span><br><br>
-    <strong>You will need: </strong><span class='ingredients recipe-info'></span>
-    <strong>Instructions: </strong><ol><span class='instructions recipe-info'>
-    </span></ol>
-    </p>`;
+    cardArea.innerHTML = 
+      `<h3>${recipeObject.name}</h3>
+      <p class='all-recipe-info'>
+          <strong>It will cost: </strong>
+          <span class='cost recipe-info'>$${costInDollars}</span>
+          <br><br>
+        <strong>You will need: </strong>
+        <span class='ingredients recipe-info'></span>
+        <strong>Instructions: </strong>
+        <ol>
+          <span class='instructions recipe-info'>
+          </span>
+        </ol>
+      </p>`;
     let ingredientsSpan = document.querySelector('.ingredients');
     let instructionsSpan = document.querySelector('.instructions');
     // should find a way to either make these selectors global or remove them
@@ -114,9 +121,8 @@ class DomUpdates {
       `)
     })
     recipeObject.instructions.forEach(instruction => {
-      instructionsSpan.insertAdjacentHTML('beforebegin', `<li>
-      ${instruction.instruction}</li>
-      `)
+      instructionsSpan.insertAdjacentHTML('beforebegin', 
+      `<li>${instruction.instruction}</li>`)
     })
   }
 
@@ -134,26 +140,23 @@ class DomUpdates {
       cardArea.classList.remove('all')
     }
     recipes.forEach(recipe => {
-      cardArea.insertAdjacentHTML('afterbegin', `<article id='${recipe.id}-card'
-      class='card'>
+      cardArea.insertAdjacentHTML('afterbegin', 
+        `<article id='${recipe.id}-card' class='card'>
           <header id='${recipe.id}-header' class='card-header'>
-            
               <label for='add-button' class='hidden'>Click to add recipe</label>
               <button id='${recipe.id}-add' aria-label='add-button' class='add-button card-button'></button>
               <label for='favorite-button' class='hidden'>Click to favorite recipe</label>
               <button id='${recipe.id}-favorite' aria-label='favorite-button' class='favorite favorite${recipe.id} card-button'></button>
-  
-            
           </header>
-  
           <section class="card-body">
             <article id='${recipe.id}-recipie-name' class='recipe-name'>${recipe.name}
-            <img id='${recipe.id}-picture' tabindex='0' class='card-picture'
-            src='${recipe.image}' alt='click to view recipe for ${recipe.name}'>
+              <img id='${recipe.id}-picture' tabindex='0' class='card-picture' src='${recipe.image}' alt='click to view recipe for ${recipe.name}'>
             </article>
-            </section>
-            </article>`)
+          </section>
+        </article>`)
     })
     getFavorites();
   }
 }
+
+export default DomUpdates;
